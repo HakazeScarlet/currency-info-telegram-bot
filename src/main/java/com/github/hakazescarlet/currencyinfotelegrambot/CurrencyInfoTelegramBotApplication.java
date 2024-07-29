6b711224c17eeed041c6bced2046de4dffc31749
hakazescarlet <frost.scarlet.163@gmail.com>
2024-07-29 16:50:35 +0300

    initial commit

---
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/CurrencyInfoTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.hakazescarlet.currencyinfotelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyInfoTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyInfoTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hakazescarlet/currencyinfotelegrambot/CurrencyInfoTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hakazescarlet.currencyinfotelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyInfoTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
