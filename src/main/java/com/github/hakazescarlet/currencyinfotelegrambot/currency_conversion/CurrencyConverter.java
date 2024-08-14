8e0457bc8830dcd70cdbd3bef25a97f2a08a5441
HakazeScarlet <frost.scarlet.163@gmail.com>
2024-08-14 19:39:05 +0300

    Replaced from Double to BigDecimal types

--- src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyConverter.java
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyConverter.java
@@ -4,6 +4,7 @@ import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.curr
 import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.currency_api.ConversionRatesHolder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 @Service
@@ -15,10 +16,10 @@ public class CurrencyConverter {
         this.currencyConverterApiProvider = currencyConverterApiProvider;
     }
 
-    public Double convert(String current, String target, Double amount) {
+    public BigDecimal convert(String current, String target, BigDecimal amount) {
         // TODO: handle case when one of three parameters will be null (do it after telegram logic writing)
         ConversionRatesHolder holder = currencyConverterApiProvider.getExchangeRate(current);
         Map<String, Double> conversionRates = holder.getConversionRates();
-        return conversionRates.get(target) * amount;
+        return BigDecimal.valueOf(conversionRates.get(target)).multiply(amount);
     }
 }
