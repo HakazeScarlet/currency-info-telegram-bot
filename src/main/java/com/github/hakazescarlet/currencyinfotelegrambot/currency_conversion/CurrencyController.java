10c7c9a6f8c4b1bad03da9442e7f8f7ad128b021
HakazeScarlet <frost.scarlet.163@gmail.com>
2024-08-18 19:13:33 +0300

    Implemented return result as wrapped to ResponseEntity class

--- src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyController.java
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyController.java
@@ -1,32 +1,27 @@
 package com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion;
 
-import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.currency_api.BeaconCurrencyApiProvider;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping
 public class CurrencyController {
 
     private final CurrencyConverter currencyConverter;
-    private final BeaconCurrencyApiProvider beaconCurrencyApiProvider;
 
-    public CurrencyController(CurrencyConverter currencyConverter, BeaconCurrencyApiProvider beaconCurrencyApiProvider) {
+    public CurrencyController(CurrencyConverter currencyConverter) {
         this.currencyConverter = currencyConverter;
-        this.beaconCurrencyApiProvider = beaconCurrencyApiProvider;
     }
 
-    // TODO: add request parameters: current, target, amount
-    // TODO: return result as SomeObj class wrapped to ResponseEntity class
-    @GetMapping("/calc")
-    public void convert(@RequestParam String from, @RequestParam String to, @RequestParam BigDecimal amount) {
-//        currencyConverter.convert("JPY", "EUR", 3654.0);
-//        currencyBeaconApiProvider.convert("GBP", "USD", BigDecimal.valueOf(12345.0));
-//        currencyBeaconApiProvider.getCurrencyBeaconExchangeRates("USD");
-        currencyConverter.convert("GBP", "ZAR", BigDecimal.valueOf(1000.0));
+    @GetMapping("/convert")
+    public ResponseEntity<BigDecimal> convert(
+        @RequestParam String from,
+        @RequestParam String to,
+        @RequestParam Double amount) {
+        BigDecimal result = currencyConverter.convert(from, to, BigDecimal.valueOf(amount));
+        return ResponseEntity.ok(result);
     }
 }
