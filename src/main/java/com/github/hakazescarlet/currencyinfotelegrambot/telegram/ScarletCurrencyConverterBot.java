9e6e7212f9e0d712f6a5737f0380bd8ea48f9796
HakazeScarlet <frost.scarlet.163@gmail.com>
2024-08-26 01:16:53 +0300

    Implemented telegram echo bot logic

--- src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/telegram/ScarletCurrencyConverterBot.java
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/telegram/ScarletCurrencyConverterBot.java
@@ -1,9 +1,15 @@
 package com.github.hakazescarlet.currencyinfotelegrambot.telegram;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class ScarletCurrencyConverterBot extends TelegramLongPollingBot {
+
+    private static final Logger logger = LogManager.getLogger();
 
     public ScarletCurrencyConverterBot(String botToken) {
         super(botToken);
@@ -11,7 +17,26 @@ public class ScarletCurrencyConverterBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if  (update.hasMessage() && update.getMessage().hasText()) {
+            String  textFromUser  =  update.getMessage().getText();
 
+            Long  userId  =  update.getMessage().getChatId();
+            String  userFirstName  =  update.getMessage().getFrom().getFirstName();
+
+            logger.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
+
+            SendMessage sendMessage  =  SendMessage.builder()
+                .chatId(userId.toString())
+                .text( "Hello, I've received your text: "  + textFromUser)
+                .build();
+            try  {
+                this.sendApiMethod(sendMessage);
+            }  catch  (TelegramApiException e) {
+                logger.error( "Exception when sending message: " , e);
+            }
+        }  else  {
+            logger.warn( "Unexpected update from user" );
+        }
     }
 
     @Override
