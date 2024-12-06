ddd31e9f08428d9f17b9ab41f6261df4d402f109
HakazeScarlet <frost.scarlet.163@gmail.com>
2024-12-06 16:53:14 +0300

    Got rid of redundant rates api provider

--- src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyConverter.java
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/currency_conversion/CurrencyConverter.java
@@ -3,7 +3,6 @@ package com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion;
 import com.github.hakazescarlet.currencyinfotelegrambot.chat_bot_storage.entities.PairHolder;
 import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.currency_api.BeaconCurrencyApiProvider;
 import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.currency_api.BeaconExchangeRatesHolder;
-import com.github.hakazescarlet.currencyinfotelegrambot.currency_conversion.currency_api.ConversionRatesApiProvider;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -13,23 +12,14 @@ import java.util.Map;
 @Service
 public class CurrencyConverter {
 
-    private final ConversionRatesApiProvider currencyConverterApiProvider;
     private final BeaconCurrencyApiProvider beaconCurrencyApiProvider;
 
-    public CurrencyConverter(
-        ConversionRatesApiProvider currencyConverterApiProvider,
-        BeaconCurrencyApiProvider beaconCurrencyApiProvider
-    ) {
-        this.currencyConverterApiProvider = currencyConverterApiProvider;
+    public CurrencyConverter(BeaconCurrencyApiProvider beaconCurrencyApiProvider) {
         this.beaconCurrencyApiProvider = beaconCurrencyApiProvider;
     }
 
     public BigDecimal convert(PairHolder pairHolder, BigDecimal amount) {
         // TODO: handle case when one of three parameters will be null (do it after telegram logic writing)
-//        ConversionRatesHolder holder = currencyConverterApiProvider.getExchangeRate(current);
-//        Map<String, Double> conversionRates = holder.getConversionRates();
-//        return BigDecimal.valueOf(conversionRates.get(target)).multiply(amount);
-
         BeaconExchangeRatesHolder beaconExchangeRatesHolder = beaconCurrencyApiProvider.getRates(pairHolder.getCurrent());
         Map<String, Double> conversionRates = beaconExchangeRatesHolder.getRates();
         Double currencyRate = conversionRates.get(pairHolder.getTarget());
