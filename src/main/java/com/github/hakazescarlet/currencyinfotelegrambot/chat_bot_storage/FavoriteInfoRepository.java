cba32bd8969fbb4475f05dc3bc40a288aa9ee634
HakazeScarlet <frost.scarlet.163@gmail.com>
2024-12-06 16:50:15 +0300

    Added example json

--- src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/chat_bot_storage/FavoriteInfoRepository.java
+++ src/main/java/com/github/hakazescarlet/currencyinfotelegrambot/chat_bot_storage/FavoriteInfoRepository.java
@@ -1,10 +1,12 @@
 package com.github.hakazescarlet.currencyinfotelegrambot.chat_bot_storage;
 
+import com.github.hakazescarlet.currencyinfotelegrambot.chat_bot_storage.entities.UserChatInfo;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.stereotype.Repository;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @Repository
 public class FavoriteInfoRepository {
@@ -12,6 +14,7 @@ public class FavoriteInfoRepository {
     private static final String USERS_DB = "users_db";  //TODO: rename
     private static final String USERS_COLLECTION = "users_collection";
     private static final String USER_ID = "id";
+    private static final String FAVORITE_PAIRS = "favorite_pairs";
 
     private final MongoClient mongoClient;
 
@@ -20,22 +23,29 @@ public class FavoriteInfoRepository {
     }
 
     public void save(ChatInfo chatInfo) {
-        MongoDatabase database = mongoClient.getDatabase(USERS_DB);
-        MongoCollection<Document> collection = database.getCollection(USERS_COLLECTION);
+        MongoCollection<UserChatInfo> collection = mongoClient.getDatabase(USERS_DB)
+            .getCollection(USERS_COLLECTION, UserChatInfo.class);
 
+        Bson filterFields = eq(USER_ID, chatInfo.getId());
+
+
+//        List<FavoritePair> list = new ArrayList<>();
+//        FavoritePair favoritePair = new FavoritePair();
+//        favoritePair.setPairHolder(new PairHolder("5555gggg", "11111tttt"));
+//        favoritePair.setLocalDateTime(LocalDateTime.now());
+//        list.add(favoritePair);
+//
+//        Bson update = Updates.set(FAVORITE_PAIRS, list);
+//
+//        collection.updateMany(filterFields, update);
         // TODO: сохранять данные в таблицу Favorite. Колонки id, current, target, date_time (найти тип данных для монго - DateTime)
         // TODO: делать проверку на количество валютных пар в таблице Favorite (5) перед записью новой
         // TODO: если их уже 5, то перезаписать самую старую по дате
 
 //        UpdateOptions updateOptions = new UpdateOptions();
 //        updateOptions.upsert(true);
-//
-//        Bson filter = eq(ID_FIELD, chatInfo.getId());
-//        Bson update = Updates.combine(
-//            Updates.set(CURRENT_FIELD, chatInfo.getCurrent()),
-//            Updates.set(TARGET_FIELD, chatInfo.getTarget()),
-//            Updates.set(DATE_TIME, LocalDateTime.now()));
 
+//        Bson filter = eq(USER_ID, chatInfo.getId());
 //        collection.updateOne(filter, update, updateOptions);
     }
 }
